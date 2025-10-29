dfd958fdd1518cf4dd3a4f1f44eed6caa65083eb
AbdulmohsenMaj <you.1s1@hotmail.com>
2025-10-29 12:06:07 +0300

    Fix: Corrected module not found error in nextAuthOptions import

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 // app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
-import { authOptions } from "../../../lib/nextAuthOptions";
+import { authOptions } from "../../../../lib/nextAuthOptions.js";
 
 const handler = NextAuth(authOptions);
 
